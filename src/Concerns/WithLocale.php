c7299ded2d22b3d371a6628f09b5fafceddd9e31
Simon Barrett <simonbarrett@acttraining.org.uk>
2023-12-16 11:56:42 +0000

    :sparkles: add location options

--- src/Concerns/WithLocale.php
+++ src/Concerns/WithLocale.php
@@ -4,13 +4,35 @@ namespace ACTTraining\LivewireCalendar\Concerns;
 
 trait WithLocale
 {
-    protected string $locale = 'en';
+    protected ?string $locale = null;
+
+    protected ?string $timeZone = null;
 
     protected int $firstDay = 0;
 
     public function locale(): string
     {
+        if (is_null($this->locale)) {
+            return config('app.locale');
+        }
+
         return $this->locale;
+    }
+
+    public function timeZone(): string
+    {
+        if (is_null($this->timeZone)) {
+            return config('app.timezone');
+        }
+
+        return $this->timeZone;
+    }
+
+    public function setTimeZone(string $timeZone = 'local'): static
+    {
+        $this->timeZone = $timeZone;
+
+        return $this;
     }
 
     public function setLocale(string $locale = 'en'): static
